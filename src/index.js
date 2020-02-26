f13d610e668fea762630a57b1106f3171068366b
Oksana Tukach <Oshabovta@mail.ru>
2020-02-26 16:12:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(arguments.length === 0 ||array.length === 0) { 
+    return 0;
+  } else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(arguments.length === 0 ||array.length === 0) { 
+    return 0;
+  } else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(arguments.length === 0 ||array.length === 0) { 
+    return 0;
+  } else {
+        arr = array.reduce((a, b) => a + b);
+        return arr / array.length;
+  }
 }
